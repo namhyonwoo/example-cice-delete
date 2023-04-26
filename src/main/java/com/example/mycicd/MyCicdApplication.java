9e4d6cd8fd8b2a2357692dd2b13bcb63ae071435
남현우 <hy.nam@huray.net>
2023-04-26 18:35:04 +0900

    first commit

---
+++ src/main/java/com/example/mycicd/MyCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycicd/MyCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
